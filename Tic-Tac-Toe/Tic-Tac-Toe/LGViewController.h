7c9368cb7a4ae9483b12a67a67b68b6a1f6e5a0b
Holger Liesegang <hl@liesegang-media.de>
2014-03-23 09:07:18 +0100

    First fully functional version

--- Tic-Tac-Toe/Tic-Tac-Toe/LGViewController.h
+++ Tic-Tac-Toe/Tic-Tac-Toe/LGViewController.h
@@ -8,6 +8,5 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LGViewController : UIViewController
-
+@interface LGViewController : UIViewController <UIAlertViewDelegate>
 @end
--- Tic-Tac-Toe/Tic-Tac-Toe/LGViewController.m
+++ Tic-Tac-Toe/Tic-Tac-Toe/LGViewController.m
@@ -9,6 +9,24 @@
 #import "LGViewController.h"
 
 @interface LGViewController ()
+{
+    UIImageView *tmpIV;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *imageViewField;
+
+@property (weak, nonatomic) UIImage *imageForTheXSign;
+@property (weak, nonatomic) UIImage *imageForTheOSign;
+@property (weak, nonatomic) UIImage *imageForTheXredSign;
+@property (weak, nonatomic) UIImage *imageForTheOredSign;
+
+@property (copy, nonatomic) NSString *activePlayer;
+@property (assign) NSInteger countMove;
+
+-(void) startNewGame;
+-(NSString *) checkForWinner;
 
 @end
 
@@ -17,13 +35,289 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	// Assigning the O & X signs
+    self.imageForTheXSign = [UIImage imageNamed:@"X.png"];
+    self.imageForTheOSign = [UIImage imageNamed:@"O.png"];
+    self.imageForTheXredSign = [UIImage imageNamed:@"X-red.png"];
+    self.imageForTheOredSign = [UIImage imageNamed:@"O-red.png"];
+    // Set X (1) as active player
+    self.activePlayer = @"X";
+    self.statusLabel.text = @"It's you turn, X";
+    // Generate imageViewField
+    self.countMove = 1;
+    tmpIV = [UIImageView new];
+    [self startNewGame];
 }
 
-- (void)didReceiveMemoryWarning
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    UITouch *touch = [[event allTouches] anyObject];
+    // check to see which imageViewField of type UIImage has been touched
+    for (UIImageView *field in self.imageViewField)
+    {
+        if (CGRectContainsPoint(field.frame, [touch locationInView:self.view]))
+        {
+            if ([self.activePlayer isEqual:@"X"])
+            {
+                field.image = self.imageForTheXSign;
+                self.activePlayer = @"O";
+                self.statusLabel.text = @"It's you turn, O";
+            }
+            else
+            {
+                field.image = self.imageForTheOSign;
+                self.activePlayer = @"X";
+                self.statusLabel.text = @"It's you turn, X";
+            }
+        };
+    };
+    NSString *result = [self checkForWinner];
+    if ([result isEqual:@"X"])
+    {
+        self.statusLabel.text = @"";
+        UIAlertView *winnerX = [[UIAlertView alloc]initWithTitle:@"X won!" message:nil delegate:self cancelButtonTitle:@"ok" otherButtonTitles: nil];
+        [winnerX show];
+    };
+    if ([result isEqual:@"O"])
+    {
+        self.statusLabel.text = @"";
+        UIAlertView *winnerO = [[UIAlertView alloc]initWithTitle:@"O won!" message:nil delegate:self cancelButtonTitle:@"ok" otherButtonTitles: nil];
+        [winnerO show];
+    };
+    self.countMove++;
+    if (self.countMove > 9)
+    {
+        self.statusLabel.text = @"";
+        UIAlertView *noWinner = [[UIAlertView alloc]initWithTitle:@"No winner!" message:nil delegate:self cancelButtonTitle:@"ok" otherButtonTitles: nil];
+        [noWinner show];
+    };
+}
+
+
+- (void)startNewGame
+{
+    NSLog(@"StartNewGame");
+    for (UIImageView *field in self.imageViewField) {
+        field.image = NULL;
+    }
+    self.countMove = 1;
+    self.activePlayer = @"X";
+    self.statusLabel.text = @"It's you turn, X";
+}
+
+
+- (NSString *)checkForWinner {
+
+	// Check horizontal
+    if (([self.imageViewField[0] image] != NULL) &
+        ([self.imageViewField[0] image] == [self.imageViewField[1] image]) &
+        ([self.imageViewField[1] image] == [self.imageViewField[2] image]))
+    {
+        if ([[self.imageViewField[0] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[0];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[1];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[2];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[0];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[1];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[2];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+
+    if (([self.imageViewField[3] image] != NULL) &
+        ([self.imageViewField[3] image] == [self.imageViewField[4] image]) &
+        ([self.imageViewField[4] image] == [self.imageViewField[5] image]))
+    {
+        if ([[self.imageViewField[3] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[3];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[5];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[3];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[5];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+
+    if (([self.imageViewField[6] image] != NULL) &
+        ([self.imageViewField[6] image] == [self.imageViewField[7] image]) &
+        ([self.imageViewField[7] image] == [self.imageViewField[8] image]))
+    {
+        if ([[self.imageViewField[6] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[6];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[7];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[8];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[6];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[7];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[8];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+
+	// Check vertical
+    if (([self.imageViewField[0] image] != NULL) &
+        ([self.imageViewField[0] image] == [self.imageViewField[3] image]) &
+        ([self.imageViewField[3] image] == [self.imageViewField[6] image]))
+    {
+        if ([[self.imageViewField[0] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[0];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[3];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[6];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[0];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[3];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[6];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+
+    if (([self.imageViewField[1] image] != NULL) &
+        ([self.imageViewField[1] image] == [self.imageViewField[4] image]) &
+        ([self.imageViewField[4] image] == [self.imageViewField[7] image]))
+    {
+        if ([[self.imageViewField[1] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[1];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[7];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[1];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[7];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+
+    if (([self.imageViewField[2] image] != NULL) &
+        ([self.imageViewField[2] image] == [self.imageViewField[5] image]) &
+        ([self.imageViewField[5] image] == [self.imageViewField[8] image]))
+    {
+        if ([[self.imageViewField[2] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[2];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[5];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[8];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[2];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[5];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[8];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+
+	// Check diagonal
+    if (([self.imageViewField[0] image] != NULL) &
+        ([self.imageViewField[0] image] == [self.imageViewField[4] image]) &
+        ([self.imageViewField[4] image] == [self.imageViewField[8] image]))
+    {
+        if ([[self.imageViewField[0] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[0];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[8];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[0];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[8];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+
+    if (([self.imageViewField[2] image] != NULL) &
+        ([self.imageViewField[2] image] == [self.imageViewField[4] image]) &
+        ([self.imageViewField[4] image] == [self.imageViewField[6] image]))
+    {
+        if ([[self.imageViewField[2] image] isEqual: self.imageForTheXSign])
+        {
+            tmpIV = self.imageViewField[2];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheXredSign;
+            tmpIV = self.imageViewField[6];
+            tmpIV.image = self.imageForTheXredSign;
+            return @"X";
+        } else
+        {
+            tmpIV = self.imageViewField[2];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[4];
+            tmpIV.image = self.imageForTheOredSign;
+            tmpIV = self.imageViewField[6];
+            tmpIV.image = self.imageForTheOredSign;
+            return @"O";
+        };
+    };
+    
+    return nil;
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    [self startNewGame];
 }
 
 @end
